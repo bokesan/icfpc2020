94ad1706983cc0f803e7496d78c9d5c703dae5f1
Jan Dreske <jandreske@web.de>
2020-07-17 22:46:38 +0200

    added script to demodulate numbers and lists

---
+++ tools/dem.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function neg {
+	printf "-"
+	pos
+}
+
+function pos {
+	((index=index+2))
+	length=0
+	while [ "${input:$index:1}" = "1" ]; do
+		((index=index+1))
+		((length=length+4))
+	done
+	((index=index+1))
+	if [ "$length" = 0 ]; then
+		printf "0 "
+	else
+		number="${input:$index:$length}"
+		printf "$((2#$number)) "
+		((index=index+$length))
+	fi
+}
+
+function list {
+	((index=index+2))
+	printf "( "
+	check
+	while [ "${input:$index:2}" = "11" ]; do
+		((index=index+2))
+		printf ", "
+		check
+	done
+	printf ")"
+}
+
+function check {
+	case "${input:$index:2}" in
+	01)
+		pos
+		;;
+	10)
+		neg
+		;;
+	11)
+		list
+		;;
+	00)
+		printf "nil "
+		;;
+	*)
+		echo "unknown prefix $pref"
+		;;
+	esac
+}
+
+if [ -z "$1" ]; then
+	echo "pass modulated string as argument, for example: 110110000111011111100001001010110101000100"
+	exit 1	
+fi
+
+input=$1
+index=0
+check
+printf "\n"
+
+
